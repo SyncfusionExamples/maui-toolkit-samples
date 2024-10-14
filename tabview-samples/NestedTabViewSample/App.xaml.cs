c6c60037679d50ae4fe02bf045b782cf745a80ad
DeepakG <deepakguansekaran@syncfusion.com>
2024-10-14 16:43:37 +0530

    Nested tab view sample is added

---
+++ tabview-samples/NestedTabViewSample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NestedTabViewSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
