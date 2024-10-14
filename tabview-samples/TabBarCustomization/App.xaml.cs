e98a4c51c3ed9aafe673d8f719fe15dc40e849b8
DeepakG <deepakguansekaran@syncfusion.com>
2024-10-14 15:10:35 +0530

    Tab bar customization sample is added

---
+++ tabview-samples/TabBarCustomization/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TabBarCustomization
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
