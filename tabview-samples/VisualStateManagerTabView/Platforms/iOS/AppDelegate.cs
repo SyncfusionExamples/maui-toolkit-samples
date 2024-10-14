ae5f79c1afb07dd411e7e9abc59a8886f51db8ff
DeepakG <deepakguansekaran@syncfusion.com>
2024-10-14 16:34:41 +0530

    VisualStateManager sample is added for TabView

---
+++ tabview-samples/VisualStateManagerTabView/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace VisualStateManagerTabView
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
