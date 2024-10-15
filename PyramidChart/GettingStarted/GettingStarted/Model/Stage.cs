c1208e99d5d16b13f1677171bae7a5acc52d0da1
saiyathalifathimabee.m@syncfusion.com <Saiyathalif@17>
2024-10-15 18:48:55 +0530

    Added the pyramid chart sample

---
+++ PyramidChart/GettingStarted/GettingStarted/Model/Stage.cs
@@ -0,0 +1,8 @@
+﻿namespace GettingStarted
+{
+    public class Stage
+    {
+        public string? Name { get; set; }
+        public double Value { get; set; }
+    }
+}
---
+++ PyramidChart/GettingStarted/GettingStarted/ViewModel/ChartViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace GettingStarted
+{
+    public class ChartViewModel
+    {
+        public List<Stage> Data { get; set; }
+
+        public ChartViewModel()
+        {
+            Data = new List<Stage>()
+            {
+                new Stage(){Name = "Stage A", Value=12},
+                new Stage(){Name = "Stage B", Value=21},
+                new Stage(){Name = "Stage C", Value=29},
+                new Stage(){Name = "Stage D", Value=37},
+            };
+        }
+    }
+}
