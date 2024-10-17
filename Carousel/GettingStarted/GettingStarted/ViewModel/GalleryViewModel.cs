b11ddc5b8455d07dae57e7169610289304149847
ranjithsf4081 <127499234+SivaranjithN@users.noreply.github.com>
2024-10-17 15:02:46 +0530

    Prepared GettingStarted Sample for MAUI toolkit Carousel control.

---
+++ Carousel/GettingStarted/GettingStarted/ViewModel/GalleryViewModel.cs
@@ -0,0 +1,23 @@
+namespace GettingStarted;
+
+public class GalleryViewModel
+{
+    public GalleryViewModel()
+    {
+        ImageCollection.Add(new ImageModel("austria.jpg"));
+        ImageCollection.Add(new ImageModel("india.jpg"));
+        ImageCollection.Add(new ImageModel("dubai.jpg"));
+        ImageCollection.Add(new ImageModel("canada.jpg"));
+        ImageCollection.Add(new ImageModel("kremlin.jpg"));
+        ImageCollection.Add(new ImageModel("colombia.jpg"));
+        ImageCollection.Add(new ImageModel("france.jpg"));
+        ImageCollection.Add(new ImageModel("germany.jpg"));
+        ImageCollection.Add(new ImageModel("italy.jpg"));
+    }
+    private List<ImageModel> imageCollection = new List<ImageModel>();
+    public List<ImageModel> ImageCollection
+    {
+        get { return imageCollection; }
+        set { imageCollection = value; }
+    }
+}
