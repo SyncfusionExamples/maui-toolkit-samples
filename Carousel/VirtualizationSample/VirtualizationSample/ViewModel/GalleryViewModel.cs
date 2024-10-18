1e86ccaf211b4fe07ffee155278ba0375a2a8406
ranjithsf4081 <127499234+SivaranjithN@users.noreply.github.com>
2024-10-18 21:03:20 +0530

    Renamed the Model name in carousel samples.

--- Carousel/VirtualizationSample/VirtualizationSample/ViewModel/GalleryViewModel.cs
+++ Carousel/VirtualizationSample/VirtualizationSample/ViewModel/GalleryViewModel.cs
@@ -6,19 +6,19 @@ public class GalleryViewModel
     {
         for(int i = 0; i < 50; i++)
         {
-            ImageCollection.Add(new ImageModel("austria.jpg"));
-            ImageCollection.Add(new ImageModel("india.jpg"));
-            ImageCollection.Add(new ImageModel("dubai.jpg"));
-            ImageCollection.Add(new ImageModel("canada.jpg"));
-            ImageCollection.Add(new ImageModel("kremlin.jpg"));
-            ImageCollection.Add(new ImageModel("colombia.jpg"));
-            ImageCollection.Add(new ImageModel("france.jpg"));
-            ImageCollection.Add(new ImageModel("germany.jpg"));
-            ImageCollection.Add(new ImageModel("italy.jpg"));
+            ImageCollection.Add(new GalleryModel("austria.jpg"));
+            ImageCollection.Add(new GalleryModel("india.jpg"));
+            ImageCollection.Add(new GalleryModel("dubai.jpg"));
+            ImageCollection.Add(new GalleryModel("canada.jpg"));
+            ImageCollection.Add(new GalleryModel("kremlin.jpg"));
+            ImageCollection.Add(new GalleryModel("colombia.jpg"));
+            ImageCollection.Add(new GalleryModel("france.jpg"));
+            ImageCollection.Add(new GalleryModel("germany.jpg"));
+            ImageCollection.Add(new GalleryModel("italy.jpg"));
         }
     }
-    private List<ImageModel> imageCollection = new List<ImageModel>();
-    public List<ImageModel> ImageCollection
+    private List<GalleryModel> imageCollection = new List<GalleryModel>();
+    public List<GalleryModel> ImageCollection
     {
         get { return imageCollection; }
         set { imageCollection = value; }
