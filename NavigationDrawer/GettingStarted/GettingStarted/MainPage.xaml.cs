1a97a81938f5437446a21fd788a4f1b7fa02f483
Sathish-087 <sathish.selvam@syncfusion.com>
2024-10-18 19:00:56 +0530

    Address review comments

--- NavigationDrawer/GettingStarted/GettingStarted/MainPage.xaml.cs
+++ NavigationDrawer/GettingStarted/GettingStarted/MainPage.xaml.cs
@@ -1,43 +1,14 @@
-﻿using Microsoft.Maui.Platform;
-using Syncfusion.Maui.Toolkit.NavigationDrawer;
-
-namespace GettingStarted
+﻿namespace GettingStarted
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
-            List<string> list = new List<string>();
-            list.Add("Home");
-            list.Add("Profile");
-            list.Add("Inbox");
-            list.Add("Out box");
-            list.Add("Sent");
-            list.Add("Draft");
-            listView.ItemsSource = list;
         }
         private void hamburgerButton_Clicked(object sender, EventArgs e)
         {
             navigationDrawer.ToggleDrawer();
         }
-
-        private void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
-        {
-            if (e.SelectedItem.ToString() == "Home")
-                contentLabel.Text = "Home";
-            else if (e.SelectedItem.ToString() == "Profile")
-                contentLabel.Text = "Profile";
-            else if (e.SelectedItem.ToString() == "Inbox")
-                contentLabel.Text = "Inbox";
-            else if (e.SelectedItem.ToString() == "Out box")
-                contentLabel.Text = "Out box";
-            else if (e.SelectedItem.ToString() == "Sent")
-                contentLabel.Text = "Sent";
-            else if (e.SelectedItem.ToString() == "Draft")
-                contentLabel.Text = "The folder is empty";
-            navigationDrawer.ToggleDrawer();
-        }
     }
-
 }
---
+++ NavigationDrawer/GettingStarted/GettingStarted/ViewModel/ListViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace GettingStarted
+{
+    /// <summary>
+    /// View model class for providing menu item data.
+    /// </summary>
+    public class ListViewModel
+    {
+        /// <summary>
+        /// Gets or sets the collection of menu items.
+        /// </summary>
+        public List<string> MenuItems { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the ListViewModel class.
+        /// </summary>
+        public ListViewModel()
+        {
+            MenuItems = new List<string>
+            {
+                "Home", "Profile", "Inbox", "Outbox", "Sent", "Draft"
+            };
+        }
+    }
+}
