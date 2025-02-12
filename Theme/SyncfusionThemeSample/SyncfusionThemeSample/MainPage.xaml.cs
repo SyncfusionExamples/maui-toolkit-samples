082575260801df03fa23f8182abfbcdf8f9bb730
Brundha <brundha.sf3969@gmail.com>
2025-02-12 09:56:33 +0530

    Added the sample for the Theme.

---
+++ Theme/SyncfusionThemeSample/SyncfusionThemeSample/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Syncfusion.Maui.Toolkit.Themes;
+
+namespace SyncfusionThemeSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void ChangeSyncfusionControlsTheme(object sender, EventArgs e)
+        {
+            if (Application.Current != null)
+            {
+                ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+                if (mergedDictionaries != null)
+                {
+                    var theme = mergedDictionaries.OfType<SyncfusionThemeResourceDictionary>().FirstOrDefault();
+                    if (theme != null)
+                    {
+                        if (theme.VisualTheme is SfVisuals.MaterialDark)
+                        {
+                            theme.VisualTheme = SfVisuals.MaterialLight;
+                            Application.Current.UserAppTheme = AppTheme.Light;
+                        }
+                        else
+                        {
+                            theme.VisualTheme = SfVisuals.MaterialDark;
+                            Application.Current.UserAppTheme = AppTheme.Dark;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
