dc59944a0dee76c799371d7e5b886f148a4c6748
Hariram4428 <hariram.subramanian@syncfusion.com>
2025-09-12 20:05:57 +0530

    MAUI-getting-started-sample-attached

---
+++ SparkChart/GettingStarted/GettingStarted/ViewModel/SparkDataViewModel.cs
@@ -0,0 +1,31 @@
+﻿using GettingStarted.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStarted.ViewModel
+{
+    public class SparkDataViewModel
+    {
+        public List<SparkDataModel> Data { get; set; }
+
+        public SparkDataViewModel()
+        {
+            Data = new List<SparkDataModel>
+            {
+                new SparkDataModel(){ Value = 5000},
+                new SparkDataModel(){ Value = 9000},
+                new SparkDataModel(){ Value = 5000},
+                new SparkDataModel(){ Value = 0},
+                new SparkDataModel(){ Value = 3000},
+                new SparkDataModel(){ Value = -4000},
+                new SparkDataModel(){ Value = 5000},
+                new SparkDataModel(){ Value = 0},
+                new SparkDataModel(){ Value = 9000},
+                new SparkDataModel(){ Value = -9000},
+            };
+        }
+    }
+}
